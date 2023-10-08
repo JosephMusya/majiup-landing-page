8e827da093d2e6c03e80b63940adb63fbc3285f3
josephmusya <muciajoe@gmail.com>
2023-10-09 00:40:04 +0300

    Add: Image carousel component

--- src/components/about/news/News.jsx
+++ src/components/about/news/News.jsx
@@ -3,8 +3,29 @@ import { GoDash } from 'react-icons/go';
 import './news.css';
 import Slide from 'react-reveal/Slide';
 import Zoom from 'react-reveal/Zoom';
+import NewsSlider from '../../slider/news-slider/NewsSlider';
 
 const News = () => {
+
+    const news = [
+        {
+            imageUrl:'https://imgs.search.brave.com/E_s9mnD8ZxmSxHexW1UgS0q1Ah13jELeOPjKeKohfOk/rs:fit:500:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzAyLzI1LzEyLzQ4/LzM2MF9GXzIyNTEy/NDg1Nl9mN29nSlE1/MUJHSlNJZ2hJRkV0/eUpqa0tQaXJrc1RJ/Ti5qcGc', 
+            caption:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit la.'
+        },
+        {
+            imageUrl:'https://imgs.search.brave.com/d2k-PxAxuyzxkRalP_iyayHO1zeWn0zpIX-Zdt-1uSM/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTI1/NTIzNjIwNC9waG90/by90ZWNobm9sb2d5/LmpwZz9zPTYxMng2/MTImdz0wJms9MjAm/Yz1fZjFiY044OTY3/VmIyLTluSTJNOExD/T1R5TjlDSlJhQk5v/bW5KNzg2TXpZPQ',
+            caption:'Lorem ipsum dolor sit amet consectetur.'
+        },
+        {
+            imageUrl:'https://imgs.search.brave.com/Md6bknP9ji9XYZpDM05ySsfqFU_EyAm6KV9PZy7-Xdk/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93d3cu/ZGlnaXRldW0uY29t/L3dwLWNvbnRlbnQv/dXBsb2Fkcy8yMDE5/LzEwL0lvVC13YXRl/ci1tYW5hZ2VtZW50/X3NvaWxlc3MtYWdy/aWN1bHR1cmUuanBn',
+            caption:'Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
+        },
+        {
+            imageUrl:'https://imgs.search.brave.com/22eoz78Cr-6PSaJBSm3OL1qGhklnV-ci0Wq0i0KUMe4/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93d3cu/c29mdGVxLmNvbS9o/dWJmcy9CbG9nL1Nt/YXJ0JTIwV2F0ZXIl/MjBNYW5hZ2VtZW50/JTIwVXNpbmclMjBJ/b1QuanBn',
+            caption:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit labor.'
+        }
+    ]
+
     return (
         <div className='news-disp'>
             <Slide left>
@@ -18,7 +39,7 @@ const News = () => {
             <Zoom>
                 <div className='news-bottom'>
                     <div className="news-content">
-                        <img src="https://imgs.search.brave.com/zPHiqyV_uh9frNVaWEHCvJjxCxWeUF-h5QaADtT0LAE/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTcx/ODMyMjgzMS9waG90/by9tdC1mdWppLWNh/cHBlZC1maXJzdC1z/bm93LW9mLXRoZS1z/ZWFzb24uanBnP3M9/NjEyeDYxMiZ3PTAm/az0yMCZjPU9rTHFx/RG53ODBTM2xIRl94/cXZZWXFjN0RldEUt/NFZ1MDRlYnZBZ3FC/dzQ9" alt="" />
+                        <NewsSlider news={news}/>
                     </div>
                 </div>
             </Zoom>    
--- src/components/about/team/Team.jsx
+++ src/components/about/team/Team.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
-import { BsBoxArrowUpRight, BsArrowRight } from 'react-icons/bs';
+import { BsArrowRight } from 'react-icons/bs';
 import './team.css';
 import { useNavigate } from 'react-router-dom';
 import { GoDash } from 'react-icons/go';
 import Fade from 'react-reveal/Fade';
 import Zoom from 'react-reveal/Zoom';
 import RubberBand from 'react-reveal/RubberBand';
+import ImageSlider from '../../slider/image-slider/ImageSlider';
 
 const Team = () => {
     const navigate = useNavigate();
+
+    const team = [
+        {
+            imageUrl:'https://imgs.search.brave.com/6CIkMFk6flCdG-t5snSoZM_ZksKgH75XY31uZoehHzY/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9uZXRz/dG9yYWdlLWxlZ2l0/LmFrYW1haXplZC5u/ZXQvaW1hZ2VzL2Fi/YjIxZTY5YTkyN2Q2/Y2YuanBnP2ltd2lk/dGg9OTAw', 
+            role:'CEO & Founder',
+            name:'Joseph Musya'
+        },
+        {
+            imageUrl:'https://imgs.search.brave.com/WigAsBm4aYbY_qy-zhMI7Tc6ThRQmHxUPNECV--921k/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93d3cu/ZGFwcGVyY29uZmlk/ZW50aWFsLmNvbS93/cC1jb250ZW50L3Vw/bG9hZHMvMjAxOS8w/Mi9qYXktei1lMTU2/NDc2NTU3NzcwMC5q/cGc',
+            role:'Software Developer',
+            name:'Oliver Kipkemei'
+        },
+        {
+            imageUrl:'https://imgs.search.brave.com/s_5Omt8_ayJ_89cSv4G8TAMm1JTgmEKyQe9CkdV8EXw/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMuYm9yZWRwYW5k/YS5jb20vYmxvZy93/cC1jb250ZW50L3Vw/bG9hZHMvMjAyMS8w/My9vbGQtcGhvdG9z/LWZhbW91cy1wZW9w/bGUtY2VsZWJyaXRp/ZXMtNjAzZjZjODk3/ODQ1M19fNzAwLmpw/Zw',
+            role:'Embeded System Desiger',
+            name:'Zarin Tasmin'
+        },
+        {
+            imageUrl:'https://imgs.search.brave.com/EoQvRyMHPPsjZnRuzDCE2h-f73TEBnzNhyrU9_mgAdY/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2FjLzcz/LzNjL2FjNzMzYzNh/NmQ1Zjg0ZmY1Y2M3/ZWY2ZDkzMjg5NGNk/LS1jaG9jb2xhdGUt/bWFuLW1haGVyc2hh/bGEtYWxpLmpwZw',
+            role:'Co-Founder',
+            name:'Solomon Githu'
+        }
+    ]
     return (
         <div className='team'>
             <Fade left>
@@ -25,9 +49,7 @@ const Team = () => {
             <Zoom>
                 <div className='team-bottom'>
                     <div className='team-content'>
-                        <strong>CEO & Founder</strong>
-                        <img src="https://imgs.search.brave.com/s_5Omt8_ayJ_89cSv4G8TAMm1JTgmEKyQe9CkdV8EXw/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMuYm9yZWRwYW5k/YS5jb20vYmxvZy93/cC1jb250ZW50L3Vw/bG9hZHMvMjAyMS8w/My9vbGQtcGhvdG9z/LWZhbW91cy1wZW9w/bGUtY2VsZWJyaXRp/ZXMtNjAzZjZjODk3/ODQ1M19fNzAwLmpw/Zw" alt="" />
-                        <article>Joseph Musya <BsBoxArrowUpRight/> </article>                                            
+                        <ImageSlider team={team} />
                     </div>                            
                 </div>
             </Zoom>
