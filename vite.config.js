9b956bf8771b00f1d6cf9185f618ace6e441cd5e
josephmusya <muciajoe@gmail.com>
2024-05-06 14:00:18 +0300

    Add: CNAME

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/majiup-landing-page/",
+  base: "/majiup-landing-page/dist/",
   plugins: [react()],
 });
