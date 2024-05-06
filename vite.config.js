84dd55a3a7541050973cce268d9cdd8792855020
josephmusya <muciajoe@gmail.com>
2024-05-06 14:03:35 +0300

    Update: Base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/majiup-landing-page/dist/",
+  // base: "/majiup-landing-page/",
   plugins: [react()],
 });
